bacf1586c3953271a83d46628063fa0b3deb8bfc
Jonas Nunes <jonas.nunes@ufrpe.br>
2024-09-12 23:57:39 -0300

    First Commit

---
+++ src/capture_window.py
@@ -0,0 +1,42 @@
+import mss
+import cv2
+import numpy as np
+import time
+import easyocr
+
+reader = easyocr.Reader(['pt'])  # Inicialize com os idiomas desejados
+
+def capturar_area_retangular(sct, area):
+    screenshot = sct.grab(area)
+    frame = np.array(screenshot)
+    return cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR)
+
+def extrair_texto_da_imagem(imagem):
+    resultado = reader.readtext(imagem)
+    texto = ' '.join([res[1] for res in resultado])
+    return texto.strip()
+
+def capturar_salvar_e_extrair_texto(x1, y1, x2, y2, master, intervalo=0.5):
+    with mss.mss() as sct:
+        area = {"top": y1, "left": x1, "width": x2 - x1, "height": y2 - y1}
+        
+        while master.flagThreadExtracao:
+            inicio = time.time()
+            
+            frame = capturar_area_retangular(sct, area)
+            nome_arquivo = f"captura.png"
+            cv2.imwrite(nome_arquivo, frame)
+            
+            print(f"Imagem {nome_arquivo} salva.")
+            
+            texto_extraido = extrair_texto_da_imagem(frame)
+            if texto_extraido:
+                print(f"Texto extraído da imagem {nome_arquivo}:")
+                print(texto_extraido)
+                master.label_info_extraction.config(text=f"Info: {texto_extraido}")
+            else:
+                print(f"Nenhum texto encontrado na imagem {nome_arquivo}.")
+            
+            tempo_decorrido = time.time() - inicio
+            tempo_espera = max(0, intervalo - tempo_decorrido)
+            time.sleep(tempo_espera)
---
+++ src/root.py
@@ -0,0 +1,68 @@
+import tkinter as tk
+from src.capture_coord import CapturaTela, obter_coordenadas_captura
+from src.capture_window import capturar_salvar_e_extrair_texto
+import threading
+
+class AplicacaoPrincipal(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.__configsWindow()
+        self.__elementsWindow()
+
+        self.flagThreadExtracao = False
+
+        self.mainloop()
+    
+    def __configsWindow(self):
+        self.title("Captura e Extração de Texto")
+        self.resizable(False,False)
+
+        largura_janela = 300
+        altura_janela = 200
+        largura_tela = self.winfo_screenwidth()
+        altura_tela = self.winfo_screenheight()
+        
+        posicao_x = (largura_tela - largura_janela) // 2
+        posicao_y = (altura_tela - altura_janela) // 2
+        
+        self.geometry(f"{largura_janela}x{altura_janela}+{posicao_x}+{posicao_y}")
+
+    def __elementsWindow(self):
+        self.btn_capturar = tk.Button(self, text="Capturar Coordenadas", command=self.capturar_coordenadas)
+        self.btn_capturar.pack(pady=5)
+
+        self.btn_iniciar = tk.Button(self, text="Iniciar Extração de Texto", command=self.iniciar_extracao)
+        self.btn_iniciar.pack(pady=5)
+
+        self.btn_parar = tk.Button(self, text="Parar Extração de Texto", command=self.parar_extracao)
+        self.btn_parar.pack(pady=5)
+
+        self.label_info_extraction = tk.Label(self,text="Info: ")
+        self.label_info_extraction.pack(pady=5)
+
+    def capturar_coordenadas(self):
+        self.withdraw()  # Esconde a janela principal temporariamente
+        captura = CapturaTela(self)
+        captura.wait_window()  # Espera até que a janela de captura seja fechada
+        self.deiconify()  # Mostra a janela principal novamente
+
+    def iniciar_extracao(self):
+        coordenadas = obter_coordenadas_captura()
+        if coordenadas:
+            x1, y1, x2, y2 = coordenadas
+            self.withdraw()  # Esconde a janela principal
+            try:
+                self.flagThreadExtracao = True
+                thread = threading.Thread(target=lambda:capturar_salvar_e_extrair_texto(x1, y1, x2, y2,self))
+                thread.daemon = True
+                thread.start()
+            except KeyboardInterrupt:
+                print("Processo interrompido pelo usuário.")
+            finally:
+                self.deiconify()  # Mostra a janela principal novamente
+        else:
+            print("Por favor, capture as coordenadas primeiro.")
+
+    def parar_extracao(self):
+        self.flagThreadExtracao = False
